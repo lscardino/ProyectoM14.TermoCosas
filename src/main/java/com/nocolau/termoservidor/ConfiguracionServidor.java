7221f26d7de2488376aab5a10302968e63a68e93
5hinko <se4bzhang@gmail.com>
2019-05-16 13:36:30 +0200

    Clase nueva, guarda la configuración del servidor como; -PUERTO -TIEMPO DE LOS BUCLES -NUM A DIVIDIR PARA MUESTRA

---
+++ src/main/java/com/nocolau/termoservidor/ConfiguracionServidor.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nocolau.termoservidor;
+
+/**
+ *
+ * @author Cho_S
+ */
+public class ConfiguracionServidor {
+
+    private int puerto = 20003;
+    private long TIEMPO_BUCLE = 30000;
+    private int TIEMPO_DIV_VARIABLE = 3;
+
+    ConfiguracionServidor() {
+    }
+
+    ConfiguracionServidor(int nPuerto) {
+        this.puerto = nPuerto;
+    }
+
+    ConfiguracionServidor(long nTiempoBucle, int nTiempoDiv) {
+        this.TIEMPO_BUCLE = nTiempoBucle;
+        this.TIEMPO_DIV_VARIABLE = nTiempoDiv;
+    }
+
+    ConfiguracionServidor(int nPuerto, long nTiempoBucle, int nTiempoDiv) {
+        this.puerto = nPuerto;
+        this.TIEMPO_BUCLE = nTiempoBucle;
+        this.TIEMPO_DIV_VARIABLE = nTiempoDiv;
+
+    }
+
+    public int getPuerto() {
+        return puerto;
+    }
+
+    public void setPuerto(int puerto) {
+        this.puerto = puerto;
+    }
+
+    public long getTIEMPO_BUCLE() {
+        return TIEMPO_BUCLE;
+    }
+
+    public void setTIEMPO_BUCLE(long TIEMPO_BUCLE) {
+        this.TIEMPO_BUCLE = TIEMPO_BUCLE;
+    }
+
+    public int getTIEMPO_DIV_VARIABLE() {
+        return TIEMPO_DIV_VARIABLE;
+    }
+
+    public void setTIEMPO_DIV_VARIABLE(int TIEMPO_DIV_VARIABLE) {
+        this.TIEMPO_DIV_VARIABLE = TIEMPO_DIV_VARIABLE;
+    }
+
+}
