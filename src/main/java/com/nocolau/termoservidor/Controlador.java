98e3514f2436370daa85a664b9dddc1e705e6e69
5hinko <se4bzhang@gmail.com>
2019-05-16 13:30:43 +0200

    Controlador donde esta el MAIN, declarado para poder usar desde terminal

--- src/main/java/com/nocolau/termoservidor/Controlador.java
+++ src/main/java/com/nocolau/termoservidor/Controlador.java
@@ -13,8 +13,35 @@ import java.io.IOException;
  * @author Cho_S
  */
 public class Controlador {
+
     public static void main(String[] args) throws IOException {
-        //ProyectoArduino.TIEMPO_BUCLE  = 60000;
-        ProyectoArduino arduino = new ProyectoArduino();
+        ConfiguracionServidor nConfig = new ConfiguracionServidor();
+        try {
+            switch (args.length) {
+                case 1:
+                    nConfig.setPuerto(Integer.parseInt(args[0]));
+                    break;
+                case 2:
+                    nConfig.setTIEMPO_BUCLE(Long.parseLong(args[0]));
+                    nConfig.setTIEMPO_DIV_VARIABLE(Integer.parseInt(args[1]));
+                    break;
+                case 3:
+                    nConfig.setPuerto(Integer.parseInt(args[0]));
+                    nConfig.setTIEMPO_BUCLE(Long.parseLong(args[1]));
+                    nConfig.setTIEMPO_DIV_VARIABLE(Integer.parseInt(args[2]));
+                    break;
+                default:
+                    throw new NumberFormatException();
+            }
+        } catch (NumberFormatException ex) {
+            System.out.println("ERROR - Valor incorrecto, el programa usara los valores predeterminados");
+            System.out.println("INFO -        Puerto        -> " + nConfig.getPuerto());
+            System.out.println("INFO -   Tiempo de datos    -> " + nConfig.getTIEMPO_BUCLE());
+            System.out.println("INFO -  Tiempo muestras(" + nConfig.getTIEMPO_DIV_VARIABLE()
+                    + ")  -> " + (nConfig.getTIEMPO_BUCLE() / nConfig.getTIEMPO_DIV_VARIABLE()) + "ms"
+            );
+        }
+        //new SkOutServidor((nConfig));
+        ProyectoArduino arduino = new ProyectoArduino(nConfig);
     }
 }
